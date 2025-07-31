0467e02049805d20fdb912cd84978010ecce25ae
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2025-07-31 17:48:05 +0300

    add duration changing button ( static so far )

--- src/components/Typing.tsx
+++ src/components/Typing.tsx
@@ -11,6 +11,7 @@ import {
 import type { SelectChangeEvent } from '@mui/material';
 import { MenuItem } from '@mui/material';
 import axios from "axios";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 export default function TypingTest() {
     const [text, setText] = useState<string>("");
@@ -22,6 +23,7 @@ export default function TypingTest() {
     const [timeElapsed, setTimeElapsed] = useState<number>(0);
     const [testingLanguage, setTestingLanguage] = useState<string>("English");
     const testingLanguagesOptions = ['English', 'Українська']
+    const [duration, setDuration] = useState<number>(30);
 
     const inputRef = useRef<HTMLInputElement | null>(null);
     const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
@@ -159,7 +161,7 @@ export default function TypingTest() {
                 Typing Speed Test
             </h5>
 
-            <div className="w-[90%] flex">
+            <div className="w-[90%] flex justify-between">
                 <div className="flex items-center">
                     <h5>Typing language:</h5>
                     <Select
@@ -188,6 +190,30 @@ export default function TypingTest() {
                         ))}
                     </Select>
                 </div>
+                <Stack direction="row" spacing={1} alignItems="center">
+                    <h5>Duration:</h5>
+                    <ButtonGroup size="small" aria-label="typing duration" >
+                        {[30, 60, 90].map((d) => (
+                            <Button
+                                key={d}
+                                variant={duration === d ? "contained" : "outlined"}
+                                onClick={() => setDuration(d)}
+                                sx={{
+                                    color: duration === d ? "#fff" : "#333",
+                                    borderColor: "#10B981",
+                                    backgroundColor: duration === d ? "#10B981" : "transparent",
+                                    "&:hover": {
+                                        backgroundColor: duration === d ? "#10B981" : "#f5f5f5",
+                                    },
+                                    fontWeight: 500
+                                }}
+                            >
+                                {d}s
+                            </Button>
+                        ))}
+                    </ButtonGroup>
+                </Stack>
+
             </div>
 
             <div className="w-[90%] mb-4 wrap-break-word bg-[#fff] rounded-xl p-4 text-[20px]">
